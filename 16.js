ceb8b95b9b5b26bae4d3d41f92c9c322feda1fb0
heomeco2014 <45783472+heomeco2014@users.noreply.github.com>
2022-01-20 14:46:18 +0700

    change js

--- 16.js
+++ 16.js
@@ -1,7 +1,7 @@
 const smallCups = document.querySelectorAll('.small-cup');
 const percentage = document.querySelector('.percentage');
 const remained = document.querySelector('.remained');
-
+const litterRemained = document.querySelector('#liter-remained');
 
 smallCups.forEach((smallCup, index) => {
     smallCup.addEventListener('click', () => hightLightCups(index))
@@ -26,14 +26,16 @@ function updateBigCups(idx) {
     percentage.innerText = `${drank.length/smallCups.length*100}%`;
     percentage.classList.add('fill');
     percentage.style.height = drank.length / smallCups.length * 100 + '%';
+    remained.innerText
     if (drank.length / smallCups.length * 100 == 100) {
         remained.style.display = 'none';
     } else {
         remained.style.display = 'block';
     }
-    if(drank.length/smallCups.length*100 == 0){
+    litterRemained.innerText = `${2 - drank.length / smallCups.length*2}L`;
+    if (drank.length / smallCups.length * 100 == 0) {
         percentage.style.opacity = 0;
-    }else{
+    } else {
         percentage.style.opacity = .8;
     }
 }
